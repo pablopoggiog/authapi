0765d12240fa2446d03b7a29c6066a9dba03cdcc
Pablo Poggio <pablopoggiog@gmail.com>
2021-02-05 22:51:26 -0300

    Add password hashing for signup route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,26 @@
 import express from "express";
+import bcrypt from "bcrypt";
 import { User } from "../models/index.js";
 
 const router = express.Router();
 
 router.get("/login", (req, res) => {});
 
-router.post("/signup", (req, res) => {});
+router.post("/signup", (req, res) => {
+  const { email, password } = req.body;
+
+  bcrypt.hash(password, 10, async (error, hash) => {
+    if (error) res.status(500).json(error);
+    else {
+      const newUser = User({ email, password: hash });
+      try {
+        const user = await newUser.save();
+        res.status(200).json(user);
+      } catch (e) {
+        res.status.json(error);
+      }
+    }
+  });
+});
 
 export default router;
